7382da78699c3d23cbea2baa423e9f6b57b9410e
gluha228 <daydreamgluha@gmail.com>
2022-02-08 16:53:01 +0200

    Repos created + num1

---
+++ src/main/kotlin/num1.kt
@@ -0,0 +1,37 @@
+import java.lang.IllegalArgumentException
+
+object Num1 {
+}
+
+fun main() {
+    println("q:")
+    val q = readLine()?.toInt() ?: return
+    if (q !in (0..500)) throw IllegalArgumentException("q")
+    var n: Int
+    var a: Int
+    var b: Int
+    var add: Int
+    val output = mutableListOf<MutableList<Int>>()
+    var s: MutableList<Int>
+    for (i in 0 until q) {
+        s = (readLine()?.split(" ")
+            ?.map { it -> it.toInt() } ?: return) as MutableList<Int>
+        a = s[0]
+        b = s[1]
+        n = s[2]
+        if (a !in (0..50) || b !in (0..50) || n !in (1..15))
+            throw IllegalArgumentException("a, b or n")
+        add = 1
+        s = mutableListOf()
+        for (j in 0 until n) {
+            a += add * b
+            add *= 2
+            s.add(a)
+        }
+        output.add(s)
+    }
+    for (i in output) {
+        println()
+        for (j in i.indices) print("${i[j]} ")
+    }
+}
